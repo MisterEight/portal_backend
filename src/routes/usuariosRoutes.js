1bd9ba9b5f62159a8635a129ab88dd1a72404865
Lisxt <alice14072005@gmail.com>
2025-04-30 18:30:48 -0300

    rota de usuarios criado, testando no insomia

---
+++ src/routes/usuariosRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const usuarioController = require('../controllers/usuariosController');
+
+// GET /Usuarios
+router.get('/', usuarioController.getAll)
+
+// POST /Usuarios
+router.post('/', usuarioController.create);
+
+// PUT /Usuarios/:id
+router.put('/:id', usuarioController.update);
+
+// DELETE /Usuarios/:id
+router.delete('/:id', usuarioController.delete);
+
+module.exports = router;
