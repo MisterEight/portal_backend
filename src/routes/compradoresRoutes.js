9eeb92114a7cca9b821e8ffc02e354401275cf71
YuriClovandi <yuriclovandi@gmail.com>
2025-05-24 10:59:47 -0300

    Criei uma pesquisa por id em licitações

--- src/routes/compradoresRoutes.js
+++ src/routes/compradoresRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const compradorController = require('../controllers/compradoresController');
 
-
 //GET / LISTA POR ID
 router.get('/:id', compradorController.getById);
 
