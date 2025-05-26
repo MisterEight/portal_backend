62e42ced07532f21509b20c19b0c1d24d4e1d2b3
Lisxt <alice14072005@gmail.com>
2025-05-26 19:24:32 -0300

    usuarios

--- src/routes/usuariosRoutes.js
+++ src/routes/usuariosRoutes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const usuarioController = require('../controllers/usuariosController');
 
+router.get('/:id', usuarioController.getById);
+
 // GET /Usuarios
 router.get('/', usuarioController.getAll)
 
