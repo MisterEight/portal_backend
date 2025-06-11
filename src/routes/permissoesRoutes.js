c1caa431ceab0c075e5144c6c8ff0159284cc29f
Leonardo Alcântara <leonardoalcantaradev@gmail.com>
2025-06-10 23:31:55 -0300

    Add full CRUD for permissao routes

--- src/routes/permissoesRoutes.js
+++ src/routes/permissoesRoutes.js
@@ -4,5 +4,9 @@ const permissaoController = require('../controllers/permissaoController');
 const ownership = require('../middleware/ownershipMiddleware');
 
 router.post('/', ownership('ADMIN'), permissaoController.create);
+router.put('/:id', ownership('ADMIN'), permissaoController.update);
+router.get('/', ownership('ADMIN'), permissaoController.getAll);
+router.get('/:id', ownership('ADMIN'), permissaoController.getById);
+router.delete('/:id', ownership('ADMIN'), permissaoController.remove);
 
 module.exports = router;
